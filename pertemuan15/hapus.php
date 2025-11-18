53184614a7da803b35b2b56054ab65e488110e15
paulwenny <pollywenny@gmail.com>
2025-11-18 15:33:36 +0800

    ADD : folder pertemuan15 berisi file latihan V17 tentang fitur login pada aplikasi yang dibuat sebagai latihan

---
+++ pertemuan15/hapus.php
@@ -0,0 +1,21 @@
+<?php
+    require 'functions.php';
+
+    $id = $_GET["id"];
+
+    if(hapus($id) > 0){
+        echo "
+                <script>
+                    alert('Data Berhasil Di Hapus!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+    } else{
+        echo "
+                <script>
+                    alert('Data Gagal Di Hapus!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+    }
+?>
