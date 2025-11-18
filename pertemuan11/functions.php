ca94f0b29b3e0bc2dfb6b511970f969390744dfb
paulwenny <pollywenny@gmail.com>
2025-11-18 10:05:20 +0800

    ADD : folder pertemuan11 berisi dile latihan V13 tentang fungsi UPDATE pada aplikasi yang dibuat sebagai latihan

---
+++ pertemuan11/functions.php
@@ -0,0 +1,63 @@
+<?php
+    // Koneksi ke database
+    $conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+    function query($query){
+        global $conn;
+        $result = mysqli_query($conn, $query);
+        $rows = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
+    function tambah($data){
+        global $conn;
+
+        $nama = htmlspecialchars($data["nama"]);
+        $nim = htmlspecialchars($data["nim"]);
+        $email = htmlspecialchars($data["email"]);
+        $jurusan = htmlspecialchars($data["jurusan"]);
+        $gambar = htmlspecialchars($data["gambar"]);
+
+        $query = "INSERT INTO mahasiswa (nama, nim, email, jurusan, gambar) VALUE(
+        '$nama', '$nim', '$email', '$jurusan', '$gambar')";
+
+        mysqli_query($conn, $query);
+
+        return mysqli_affected_rows($conn);
+    }
+
+    function hapus($id){
+        global $conn;
+        
+        mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+
+        return mysqli_affected_rows($conn);
+    }
+
+    function ubah($data){
+        global $conn;
+
+        $id = $data["id"];
+        $nama = htmlspecialchars($data["nama"]);
+        $nim = htmlspecialchars($data["nim"]);
+        $email = htmlspecialchars($data["email"]);
+        $jurusan = htmlspecialchars($data["jurusan"]);
+        $gambar = htmlspecialchars($data["gambar"]);
+
+        $query = "UPDATE mahasiswa SET
+                    nama = '$nama', 
+                    nim = '$nim', 
+                    email = '$email', 
+                    jurusan = '$jurusan', 
+                    gambar = '$gambar'
+                WHERE id = $id
+                ";
+
+        mysqli_query($conn, $query);
+
+        return mysqli_affected_rows($conn);
+    }
+?>
---
+++ pertemuan11/ubah.php
@@ -0,0 +1,73 @@
+<?php
+    require 'functions.php';
+
+    // Ambil data di URL
+    $id = $_GET["id"];
+
+    // Queri data mahaiswa berdasarkan id
+    $mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+    
+    
+    // Cek apakah tombol submit sudah ditekan atau belum
+    if(isset($_POST["submit"])){
+        // CEk data berhasil ditambahkan
+        if(ubah($_POST) > 0){
+            echo "
+                <script>
+                    alert('Data Berhasil Di Ubah!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+        }else{
+            echo "
+                <script>
+                    alert('Data Gagal Di Ubah!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Ubah Data Mahasiswa</h1>
+
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" require value="<?= $mhs["nama"]; ?>">
+            </li>
+            <li>
+                <label for="nim">NIM : </label>
+                <input type="text" name="nim" value="<?= $mhs["nim"]; ?>">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" value="<?= $mhs["email"]; ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" value="<?= $mhs["jurusan"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" value="<?= $mhs["gambar"]; ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit"> Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
