7db9dec7346f7c0ac1b5d5e514cb67bda9660ca4
paulwenny <pollywenny@gmail.com>
2025-11-19 18:15:09 +0800

    ADD : folder pertemuan19 copy from folder pertemuan17. folder pertemuan19 berisi file latihan V21 tentang implementasi fitur live search menggunakan ajax pada aplikasi yang dibuat sebagai latihan

---
+++ pertemuan19/note19.php
@@ -0,0 +1,9 @@
+## Belajar PHP untuk PEMULA | 21. LIVE SEARCH (menggunakan AJAX)
+
+=> Live Search
+    > Otomatis tampil
+    > fitur search google
+
+=> ajax
+    > Asynchronous Javascript and XML
+    > object XMLHttpRequest() ada didalam browser
