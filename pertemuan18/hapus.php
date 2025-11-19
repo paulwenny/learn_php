278698ee78b319fad6b32e82b6456b960a9a54aa
paulwenny <pollywenny@gmail.com>
2025-11-19 16:59:09 +0800

    ADD : folder pertemuan18 berisi file latihan V20 tentang fungsi pagination pada aplikasi yang sedang dibuat sebagai latihan << ques implementasi fungsi pagination di fitur pencaharian>>

---
+++ pertemuan18/hapus.php
@@ -0,0 +1,27 @@
+<?php
+    require 'functions.php';
+    session_start();
+
+if(!isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
+
+    $id = $_GET["id"];
+
+    if(hapus($id) > 0){
+        echo "
+                <script>
+                    alert('Data Berhasil Di Hapus!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+    } else{
+        echo "
+                <script>
+                    alert('Data Gagal Di Hapus!!!');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+    }
+?>
---
+++ pertemuan18/index.php
@@ -0,0 +1,95 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
+    // Memanggil file function.php
+    require 'functions.php';
+
+    // pagination
+    // konfigurasi
+    $jumlahDataPerhalaman = 2;
+    $jumlahData = count(query("SELECT * FROM mahasiswa"));
+    $jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
+    $halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+    $awalData = ($jumlahDataPerhalaman * $halamanAktif) - $jumlahDataPerhalaman;
+
+    $mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerhalaman");
+
+    // tombol cari ditekan
+    if(isset($_POST["cari"])){
+        $mahasiswa = cari($_POST["keyword"]);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <a href="logout.php">logout</a>
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br></br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword pencarian.." autocomplete="off">
+        <button type="submit" name="cari">Cari</button>
+    </form>
+    <br>
+
+    <!-- navigasi -->
+    <?php if($halamanAktif > 1) : ?>
+        <a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+        <?php if($i == $halamanAktif) : ?>
+            <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+        <?php else : ?>
+            <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+        <?php endif; ?>    
+    <?php endfor; ?>
+
+    <?php if($halamanAktif < $jumlahHalaman) : ?>
+        <a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo;</a>
+    <?php endif; ?>
+
+    <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th> 
+        </tr>
+        <?php $i = 1; ?>
+        <?php foreach($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $i++; ?></td>
+                <td>
+                    <a href="ubah.php?id=<?=$row["id"];?>">Ubah</a> | 
+                    <a href="hapus.php?id=<?=$row["id"];?>" onclick="return confirm('yakin?');">Hapus</a>
+                </td>
+                <td>
+                    <img src="img/<?=$row["gambar"]?>" width="50px">
+                </td>
+                <td><?=$row["nim"]?></td>
+                <td><?=$row["nama"]?></td>
+                <td><?=$row["email"]?></td>
+                <td><?=$row["jurusan"]?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
